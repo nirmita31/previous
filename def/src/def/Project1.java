1f81d07b277c16c9fce5a944409cbe6aecf6caef
nirmita31 <nirmitabristy@gmail.com>
2021-12-19 14:07:35 -0500

    firstt

---
+++ def/src/def/Project1.java
@@ -0,0 +1,11 @@
+package def;
+
+public class Project1 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chromrdriver", "C:/driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		webdriver.getproperty.chromedriver.exe"
+	
+	}
+}
